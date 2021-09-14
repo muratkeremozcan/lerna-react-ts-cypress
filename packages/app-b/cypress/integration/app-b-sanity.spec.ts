6fcc970ad578d65640dd8e8aab63eaef5c43ebb0
Murat Ozcan <muratkerem@gmail.com>
2021-09-14 10:42:15 -0500

    used different kind of contains

--- packages/app-b/cypress/integration/app-b-sanity.spec.ts
+++ packages/app-b/cypress/integration/app-b-sanity.spec.ts
@@ -2,6 +2,7 @@ describe('app-b', () => {
   it('passes sanity', () => {
     cy.visit('/');
     cy.get('.App-header').should('be.visible');
+    cy.get('p').should('be.visible');
     cy.contains('This is app-b');
   })
 })
--- packages/app-c/cypress/integration/app-c-sanity.spec.ts
+++ packages/app-c/cypress/integration/app-c-sanity.spec.ts
@@ -2,6 +2,7 @@ describe('app-c', () => {
   it('passes sanity', () => {
     cy.visit('/');
     cy.get('.App-header').should('be.visible');
+    cy.get('p').should('be.visible');
     cy.contains('This is app-a');
   })
 })
