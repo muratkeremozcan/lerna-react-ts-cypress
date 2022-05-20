9a7683b15d845a7db597e0ca8fbf3b1497f44dc8
Timmy <timmymakwan@gmail.com>
2022-05-20 14:09:40 -0700

    update test

--- packages/app-a/cypress/integration/url-visit.spec.ts
+++ packages/app-a/cypress/integration/url-visit.spec.ts
@@ -1,5 +1,5 @@
 describe('app-a', { tags: '@a' }, () => {
-    it('navigates to correct URL', () => {
+    it(`navigates to correct URL ${Cypress.env('URL_TEST')}`, () => {
       cy.visit(Cypress.env('URL_TEST'))
     })
   })
